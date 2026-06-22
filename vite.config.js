dfac1b055e72302d8b07ff3e777941d574aceff8
Leo15th <pwaiag2023@gmail.com>
2026-06-22 16:28:33 +0700

    live github and disconnect from netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',   // repo name for GitHub Pages
+  base: '/Joy-Cafe-.git',   // repo name for GitHub Pages
   plugins: [tailwindcss()],
 })
